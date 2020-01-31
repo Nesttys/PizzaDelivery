85c5ea99dd1830c497530c7d80dbfff50023ef79
SofiaFish <[soniarozhko@gmail.com]>
2020-01-31 09:22:18 +0200

    initial commit

---
+++ app/src/main/java/com/example/myapplication/PizzaAdapter.java
@@ -0,0 +1,60 @@
+package com.example.myapplication;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.squareup.picasso.Picasso;
+
+import java.util.List;
+
+public class PizzaAdapter extends RecyclerView.Adapter<PizzaAdapter.MyViewHolder> {
+
+
+    List<Pizza> list;
+
+    public PizzaAdapter(List<Pizza> list) {
+        this.list = list;
+    }
+
+    @NonNull
+    @Override
+    public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.card_view,parent,false);
+        return new MyViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
+        Picasso.get().load(list.get(position).getPhotoId()).into(holder.image);
+        holder.name.setText(list.get(position).getName());
+        holder.components.setText(list.get(position).getComponents());
+        holder.price.setText(list.get(position).getPrice() + "");
+    }
+
+    @Override
+    public int getItemCount() {
+        return list.size();
+    }
+
+    public static class MyViewHolder extends RecyclerView.ViewHolder{
+        ImageView image;
+        TextView name;
+        TextView components;
+        TextView price;
+
+        public MyViewHolder(@NonNull View itemView) {
+            super(itemView);
+            image = itemView.findViewById(R.id.pizza_photo);
+            name = itemView.findViewById(R.id.pizza_name);
+            components = itemView.findViewById(R.id.pizza_components);
+            price = itemView.findViewById(R.id.pizza_price);
+        }
+    }
+
+}
